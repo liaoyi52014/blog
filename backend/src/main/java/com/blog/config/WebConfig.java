a8e55c8c15b745fe6a1b52e08ea88eebf0257238
deanliao37 <deanliao37@gmail.com>
2026-02-24 22:29:37 +0800

    提交

--- backend/src/main/java/com/blog/config/WebConfig.java
+++ backend/src/main/java/com/blog/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.blog.config;
 
+import org.springframework.lang.NonNull;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/api/**")
             .allowedOrigins("http://localhost:3000", "http://localhost:5173")
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
